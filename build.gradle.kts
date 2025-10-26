d91ef8107c812d51bad2ac73983bf32d027f97b1
Anhuar Ruiz <myclxss@outlook.com>
2025-10-26 15:10:47 -0600

    Start project

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java")
+}
+
+group = "dev.anhuar"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:6.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
