a88bb3f3908e0496fc771e9d4cdf62e9c8534c97
Anhuar Ruiz <anhuar@MacBook-Air-de-Anhuar.local>
2025-10-27 09:30:48 -0600

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,22 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:6.1.0") 
+    implementation("net.dv8tion:JDA:6.1.0")
+
+    //ANOTATIONS
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+    //DB MANAGER
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.mongodb:mongodb-driver-sync:5.2.1")
+
+    //MANAGE FILES
+    implementation("org.yaml:snakeyaml:2.0")
+
+    //LIB COMMANDS
+    implementation("io.github.revxrsal:lamp.common:v4.0.0-rc.14")
+    implementation("io.github.revxrsal:lamp.jda:v4.0.0-rc.14")
 }
 
 tasks.test {
---
+++ src/main/java/dev/anhuar/InfinityBot.java
@@ -0,0 +1,18 @@
+package dev.anhuar;
+
+/*
+ * ========================================================
+ * InfinityBot - InfinityBot.java
+ *
+ * @author Anhuar Ruiz | Anhuar Dev | myclass
+ * @web https://anhuar.dev
+ * @date 27/10/25
+ *
+ * License: MIT License - See LICENSE file for details.
+ * Copyright (c) 2025 Anhuar Dev. All rights reserved.
+ * ========================================================
+ */
+
+public class InfinityBot {
+
+}
